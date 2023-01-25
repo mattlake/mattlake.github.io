d438f6095bb7c69534a8d314e5a7e3531ae547c8
Matthew Lake <matt@now.home>
2023-01-25 20:41:42 +0000

    Add social links

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -54,12 +54,12 @@
         <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
             <ul class="flex flex-col md:flex-row justify-center list-none">
                 <li class="md:mr-2">
-                    &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
+                    &copy; Matthew Lake {{ date('Y') }}.
                 </li>
 
                 <li>
-                    Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
-                    and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
+                    <a href="https://twitter.com/domattr">Twitter</a>
+                    <a href="https://hachyderm.io/@mattlake">Mastodon</a>
                 </li>
             </ul>
         </footer>
